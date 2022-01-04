aa2f0c98173951f64a7f37177e32072a139594cc
Khiem Vo <khiem@ipaymy.com>
2022-01-04 21:14:28 +0700

    add some pallet implementations

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -4,17 +4,44 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{
-		sp_runtime::traits::{Hash, Zero},
-		dispatch::{DispatchResultWithPostInfo, DispatchResult},
-		traits::{Currency, ExistenceRequirement, Randomness},
-		pallet_prelude::*
-	};
+	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use frame_support::{
+		sp_runtime::traits::Hash,
+		traits::{ Randomness, Currency, tokens::ExistenceRequirement },
+		transactional,
+	};
+	use sp_io::hashing::blake2_128;
+	use scale_info::TypeInfo;
 
-	// TODO Part II Struct for holding kitty information
+	#[cfg(feature = "std")]
+	use frame_support::serde::{Deserialize, Serialize};
 
-	// TODO Part II Enum and implementation to handle Gender type in Kitty struct
+	// ACTION #1: Write a Struct to hold Kitty information.
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	// Struct for holding Kitty information
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Kitty<T: Config> {
+		pub dna: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub gender: Gender,
+		pub owner: AccountOf<T>,
+	}
+
+	// Action #2: Enum declaration for Gender.
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+	pub enum Gender {
+		Male,
+		Female,
+	}
+
+	// Action #3: Implementation to handle Gender type in Kitty struct.
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -25,11 +52,19 @@ pub mod pallet {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
 		type Currency: Currency<Self::AccountId>;
+
+		// ACTION #5: Specify the type for Randomness we want to specify for runtime.
+		type KittyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+		// ACTION #9: Add MaxKittyOwned constant
+
+		#[pallet::constant]
+		type MaxKittyOwned: Get<u32>;
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		// TODO part II
+		// TODO part III
 	}
 
 	#[pallet::event]
@@ -44,28 +79,65 @@ pub mod pallet {
 	/// Keeps track of the number of Kitties in existence
 	pub(super) type KittyCnt<T: Config> = StorageValue<_, u64, ValueQuery>;
 
-	// TODO Part II: Remaining storage items.
+	// ACTION #7: Remaining storage items
+	#[pallet::storage]
+	#[pallet::getter(fn kitties)]
+	pub(super) type Kitties<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::Hash,
+		Kitty<T>,
+	>;
 
-	// TODO Part III: Our pallet's genesis configuration.
+	#[pallet::storage]
+	#[pallet::getter(fn kitties_owned)]
+	pub(super) type KittiesOwned<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<T::Hash, T::MaxKittyOwned>,
+		ValueQuery,
+	>;
+
+
+	// TODO Part IV: Our pallet's genesis configuration.
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		// TODO Part III: create kitty
 
-		// TODO Part III: set_price
+		// TODO Part IV: set_price
 
-		// TODO Part III: transfer
+		// TODO Part IV: transfer
 
-		// TODO Part III: buy_kitty
+		// TODO Part IV: buy_kitty
 
-		// TODO Part III: breed_kitty
+		// TODO Part IV: breed_kitty
 	}
 
-	// TODO Part II: helper function for Kitty struct
-
 	impl<T: Config> Pallet<T> {
+		// ACTION #4: helper function for Kitty struct
+		fn gen_gender() -> Gender {
+			let random = T::KittyRandomness::random(&b"gender"[..]).0;
+			match random.as_ref()[0] % 2 {
+				0 => Gender::Male,
+				_ => Gender::Female,
+			}
+		}
+
 		// TODO Part III: helper functions for dispatchable functions
 
-		// TODO: increment_nonce, random_hash, mint, transfer_from
+		// ACTION #6: function to randomly generate DNA
+		fn gen_dna() -> [u8; 16] {
+			let payload = (
+					T::KittyRandomness::random(&b"dna"[..]).0,
+					<frame_system::Pallet<T>>::block_number(),
+				);
+			payload.using_encoded(blake2_128)
+		}
+
+		// TODO Part III: mint
+
+		// TODO Part IV: transfer_kitty_to
 	}
 }
